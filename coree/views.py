38324417ba5be7ddacea7a591165d9c9bd8de45a
hamidusman <abdulhamidusman218@gmail.com>
2024-08-29 00:32:26 +0100

    functional ordering

--- coree/views.py
+++ coree/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Category, Expense
 from .serializers import CategorySerializer, ExpenseSerializer
-from rest_framework import generics
+from rest_framework import generics, filters
 
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import ExpenseFilter
@@ -14,6 +14,9 @@ class CategoryView(generics.ListCreateAPIView):
 class ExpenseView(generics.ListCreateAPIView):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-    filter_backends = [DjangoFilterBackend]
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = ExpenseFilter
 
+    ordering_fields = '__all__'
+    ordering = ['date']
